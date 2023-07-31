ae2b61c608554984646209dddfade10e4d230aea
Tom Ralph <tom.ralph@thevsg.co>
2023-07-31 08:28:26 -0600

    update some comments, fix the for loop

--- query_airthings.py
+++ query_airthings.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 authUrl = "https://accounts-api.airthings.com/v1/token"
 
+# Setup the Client secrets
 authPayload = json.dumps({
   "grant_type": "client_credentials",
   "client_id": "{$ CLIENT ID }",
@@ -17,8 +18,8 @@ authHeaders = {
 }
 
 deviceUrl = "https://ext-api.airthings.com/v1/devices"
-# statsUrl = "https://ext-api.airthings.com/v1/devices/" + deviceid + "/latest-samples"
 
+# Login
 auth = requests.request("POST", authUrl, headers=authHeaders, data=authPayload, timeout=3)
 authJSON = auth.json()
 authToken = authJSON.get('access_token')
@@ -27,20 +28,24 @@ requestHeader = {
   "Authorization": 'Bearer ' + authToken
 }
 
+# Get the device list
 devices = requests.get(url=deviceUrl, headers = requestHeader)
 devicesJSON = devices.json()
 
 for deviceIds in devicesJSON["devices"]:
-    if deviceIds["deviceType"] == "WAVE_PLUS":
-        deviceid = deviceIds["id"]
-        break
+  if deviceIds["deviceType"] == "WAVE_PLUS":
+    deviceid = deviceIds["id"]
 
-#print(deviceid)
+    # Query for the devices stats
+    stats = requests.get(url='https://ext-api.airthings.com/v1/devices/' + deviceid + '/latest-samples', headers = requestHeader)
+    statsJSON = stats.json()
 
-stats = requests.get(url='https://ext-api.airthings.com/v1/devices/' + deviceid + '/latest-samples', headers = requestHeader)
-statsJSON = stats.json()
+    # Format the data for influxDB, adding the deviceId to the output
+    data = statsJSON["data"]
+    data["deviceid"] = int(deviceid)
 
-data = statsJSON["data"]
-data["deviceid"] = int(deviceid)
+    # Output the data
+    print(json.dumps(data))
 
-print(json.dumps(data))
+  else:
+    print("No WAVE PLUS devices found.")
